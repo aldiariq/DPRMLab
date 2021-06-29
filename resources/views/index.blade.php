8a3104d663b1ee47aa7543c464da8efb73055a6d
aldiariq <aldi48767@gmail.com>
2021-06-29 21:56:48 +0700

    fix index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>DPMMLab Website</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-
-  <!-- Favicons -->
-  <link href="assets/img/logoutama.png" rel="icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Appland - v4.3.0
-  * Template URL: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-</head>
-
+@include('template_index.header')
 <body>
 
   <!-- ======= Header ======= -->
@@ -73,13 +38,13 @@
             <div class="row">
               <div class="col-lg-12">
                 <div class="row">
-                  <div class="col-lg-4 mt-3">
+                  <div class="col-lg-4">
                     <a href="#" class="download-btn"><i class="bx bx-book"></i> Data Science</a>
                   </div>
-                  <div class="col-lg-4 mt-3">
+                  <div class="col-lg-4">
                     <a href="#" class="download-btn"><i class="bx bx-message-alt-dots"></i> Machine Learning</a>
                   </div>
-                  <div class="col-lg-4 mt-3">
+                  <div class="col-lg-4">
                     <a href="#" class="download-btn"><i class="bx bx-lock"></i> Cryptography</a>
                   </div>
                 </div>
@@ -291,86 +256,4 @@
 
   </main><!-- End #main -->
 
-
-  <!-- ======= Footer ======= -->
-  <footer id="footer">
-    <div class="footer-newsletter">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-6">
-            <h4>Saran atau Masukan</h4>
-            <p>Silahkan kirim saran atau masukan Anda demi kemajuan grup riset kami.</p>
-            <form action="" method="post">
-              <input type="text" name="saranataumasukan"><input type="submit" value="Kirim">
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-6 col-md-6 footer-contact">
-            <h3>DPRMLab</h3>
-            <p>
-              Fakultas Ilmu Komputer Universitas Sriwijaya <br>
-              Jl. Raya Palembang - Prabumulih <br>
-              Km. 32 Indralaya, Ogan Ilir, <br>
-              Sumatera Selatan <br><br>
-              <strong>Email :</strong> dprmlab2020@gmail.com <br>
-            </p>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Topik Penelitian Kami</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Data Science</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Machine Learning</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Cryptography</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Tetap Terhubung Dengan Kami</h4>
-            <p>Berikut tautan sosial media dari grup riset kami.</p>
-            <div class="social-links mt-3">
-              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-              <a href="#" class="github"><i class="bx bxl-github"></i></a>
-              <a href="mailto:dprmlab2020@gmail.com" class="github"><i class="bx bx-mail-send"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="container py-4">
-      <div class="copyright">
-        Copyright &copy; 2021 <strong><span>DPRMLab</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/ -->
-        Web Template Inspired by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
-
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-
-</body>
-
-</html>
+@include('template_index.footer')
