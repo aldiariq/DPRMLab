6b8b660dc46ab7f280ebcd46840f62f9a2e8f9fe
aldiariq <aldi48767@gmail.com>
2021-06-29 21:01:50 +0700

    init project & create index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>DPMMLab Website</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/logoutama.png" rel="icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Appland - v4.3.0
+  * Template URL: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top  header-transparent ">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo mt-3 ">
+        <a href="index.html"><img src="assets/img/logoutama.png" alt="" class="img-fluid mb-2"></a>
+        <!-- <h1><a href="index.html" class="fw-bold">DPRMLab</a></h1> -->
+        <!-- Uncomment below if you prefer to use an image logo -->
+      </div>
+
+      <nav id="navbar" class="navbar fw-bold">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Halaman Utama</a></li>
+          <li><a class="nav-link scrollto" href="#dosenpembimbing">Dosen Pembimbing</a></li>
+          <li><a class="nav-link scrollto" href="#penelitiankami">Penelitian Kami</a></li>
+          <li><a class="nav-link scrollto" href="#anggotakami">Anggota Kami</a></li>
+          <li><a class="nav-link scrollto" href="#jadwalbimbingan">Jadwal Bimbingan</a></li>
+          <li><a class="getstarted scrollto" href="#">Halaman Admin</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-5 pt-lg-0 order-2 order-lg-1" data-aos="fade-up">
+          <div>
+            <h1>Data Science and Machine Learning Research Laboratory</h1>
+            <h2>Penjelasan DPRMLab</h2>
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="row">
+                  <div class="col-lg-4 mt-3">
+                    <a href="#" class="download-btn"><i class="bx bx-book"></i> Data Science</a>
+                  </div>
+                  <div class="col-lg-4 mt-3">
+                    <a href="#" class="download-btn"><i class="bx bx-message-alt-dots"></i> Machine Learning</a>
+                  </div>
+                  <div class="col-lg-4 mt-3">
+                    <a href="#" class="download-btn"><i class="bx bx-lock"></i> Cryptography</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 d-lg-flex flex-lg-column align-items-stretch order-1 order-lg-2 hero-img" data-aos="fade-up">
+          <img src="assets/img/gambarheader.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Dosen Pembimbing Section ======= -->
+    <section id="dosenpembimbing" class="testimonials section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Dosen Pembimbing</h2>
+          <p>Berikut Dosen Pembimbing Mahasiswa Yang Tergabung Dalam Grup Riset Penelitian DPRMLab.</p>
+        </div>
+
+        <div class="testimonials-slider swiper-container" data-aos="fade-up" data-aos-delay="500">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="assets/img/pakjulian.jpg" class="testimonial-img" alt="">
+                <h3>Julian Supardi, M.T.</h3>
+                <h4>Dosen Pembimbing</h4>
+                <p>
+                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Kata Sambutan
+                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                </p>
+              </div>
+            </div>
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="assets/img/paksamsuryadi.jpg" class="testimonial-img" alt="">
+                <h3>Syamsuryadi, M.Kom., Ph.D.</h3>
+                <h4>Dosen Pembimbing</h4>
+                <p>
+                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Kata Sambutan
+                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </section><!-- End Dosen Pembimbing Section -->
+
+    <!-- ======= Penelitian Section ======= -->
+    <section id="penelitiankami" class="details">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Penelitian Kami</h2>
+          <p>Berikut Hasil Penelitian yang Telah dan Sedang Dilaksanakan.</p>
+        </div>
+
+        <div class="row content">
+          <div class="col-md-6" data-aos="fade-right">
+            <img src="assets/img/gambarpenelitian.png" class="img-fluid img-thumbnail" alt="">
+          </div>
+          <div class="col-md-6 pt-4" data-aos="fade-up">
+            <h3>Judul Penelitian.</h3>
+            <p><i class="bi bi-people"></i> Nama Peneliti</p>
+            <p><i class="bi bi-calendar-date"></i> Tanggal Penelitian</p>
+            <p><i class="bi bi-layout-sidebar"></i> Status penelitian</p>
+            <p><i class="bi bi-card-heading"></i> Penjelasan singkat penelitian</p>
+          </div>
+        </div>
+
+        <div class="row content">
+          <div class="col-md-6 order-1 order-md-2" data-aos="fade-left">
+            <img src="assets/img/gambarpenelitian.png" class="img-fluid img-thumbnail" alt="">
+          </div>
+          <div class="col-md-6 pt-4" data-aos="fade-up">
+            <h3>Judul Penelitian.</h3>
+            <p><i class="bi bi-people"></i> Nama Peneliti</p>
+            <p><i class="bi bi-calendar-date"></i> Tanggal Penelitian</p>
+            <p><i class="bi bi-layout-sidebar"></i> Status penelitian</p>
+            <p><i class="bi bi-card-heading"></i> Penjelasan singkat penelitian</p>
+          </div>
+        </div>
+
+        <section class="faq section-bg mt-5">
+          <div class="container" data-aos="fade-up">
+            <div class="accordion-list">
+              <ul>
+                <li data-aos="fade-up" data-aos-delay="100">
+                  <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#accordion-list-2" class="collapsed">Lihat Penelitian Kami Selengkapnya. <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                  <div id="accordion-list-2" class="collapse" data-bs-parent=".accordion-list">
+                    <section class="details">
+                      <div class="container">
+                        <div class="row content">
+                          <div class="col-md-6" data-aos="fade-right">
+                            <img src="assets/img/gambarpenelitian.png" class="img-fluid img-thumbnail" alt="">
+                          </div>
+                          <div class="col-md-6 pt-4" data-aos="fade-up">
+                            <h3>Judul Penelitian.</h3>
+                            <p><i class="bi bi-people"></i> Nama Peneliti</p>
+                            <p><i class="bi bi-calendar-date"></i> Tanggal Penelitian</p>
+                            <p><i class="bi bi-layout-sidebar"></i> Status penelitian</p>
+                            <p><i class="bi bi-card-heading"></i> Penjelasan singkat penelitian</p>
+                          </div>
+                        </div>
+
+                        <div class="row content">
+                          <div class="col-md-6 order-1 order-md-2" data-aos="fade-left">
+                            <img src="assets/img/gambarpenelitian.png" class="img-fluid img-thumbnail" alt="">
+                          </div>
+                          <div class="col-md-6 pt-4" data-aos="fade-up">
+                            <h3>Judul Penelitian.</h3>
+                            <p><i class="bi bi-people"></i> Nama Peneliti</p>
+                            <p><i class="bi bi-calendar-date"></i> Tanggal Penelitian</p>
+                            <p><i class="bi bi-layout-sidebar"></i> Status penelitian</p>
+                            <p><i class="bi bi-card-heading"></i> Penjelasan singkat penelitian</p>
+                          </div>
+                        </div>
+                      </div>
+                    </section>
+                  </div>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+        </section>
+      </div>
+    </section><!-- End Penelitian Section -->
+
+    <!-- ======= Anggota Kami Section ======= -->
+    <section id="anggotakami" class="testimonials section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Anggota Kami</h2>
+          <p>Berikut Mahasiswa Yang Tergabung Dalam Grup Riset Penelitian DPRMLab.</p>
+        </div>
+
+        <div class="testimonials-slider swiper-container" data-aos="fade-up" data-aos-delay="100">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="assets/img/gambaranggota.png" class="testimonial-img" alt="">
+                <h3>Nama Anggota</h3>
+                <h3>NIM Anggota</h3>
+                <p>
+                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Kata Sambutan
+                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                </p>
+              </div>
+            </div>
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="assets/img/gambaranggota.png" class="testimonial-img" alt="">
+                <h3>Nama Anggota</h3>
+                <h3>NIM Anggota</h3>
+                <p>
+                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Kata Sambutan
+                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                </p>
+              </div>
+            </div>
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="assets/img/gambaranggota.png" class="testimonial-img" alt="">
+                <h3>Nama Anggota</h3>
+                <h3>NIM Anggota</h3>
+                <p>
+                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Kata Sambutan
+                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </section><!-- End Anggota Kami Section -->
+
+    <!-- ======= Jadwal Bimbingan Section ======= -->
+    <section id="jadwalbimbingan" class="faq">
+      <div class="container" data-aos="fade-up">
+        <div class="section-title">
+          <h2>Jadwal Bimbingan</h2>
+          <p>Berikut Jadwal Bimbingan yang Akan Datang pada Grup Riset Kami.</p>
+          <div class="section-bg mt-3">
+            <p>Tabel</p>
+          </div>
+        </div>
+      </div>
+    </section><!-- End Jadwal Bimbingan Section -->
+
+  </main><!-- End #main -->
+
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-newsletter">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6">
+            <h4>Saran atau Masukan</h4>
+            <p>Silahkan kirim saran atau masukan Anda demi kemajuan grup riset kami.</p>
+            <form action="" method="post">
+              <input type="text" name="saranataumasukan"><input type="submit" value="Kirim">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-6 col-md-6 footer-contact">
+            <h3>DPRMLab</h3>
+            <p>
+              Fakultas Ilmu Komputer Universitas Sriwijaya <br>
+              Jl. Raya Palembang - Prabumulih <br>
+              Km. 32 Indralaya, Ogan Ilir, <br>
+              Sumatera Selatan <br><br>
+              <strong>Email :</strong> dprmlab2020@gmail.com <br>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Topik Penelitian Kami</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Data Science</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Machine Learning</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Cryptography</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Tetap Terhubung Dengan Kami</h4>
+            <p>Berikut tautan sosial media dari grup riset kami.</p>
+            <div class="social-links mt-3">
+              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              <a href="#" class="github"><i class="bx bxl-github"></i></a>
+              <a href="mailto:dprmlab2020@gmail.com" class="github"><i class="bx bx-mail-send"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container py-4">
+      <div class="copyright">
+        Copyright &copy; 2021 <strong><span>DPRMLab</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/ -->
+        Web Template Inspired by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ resources/views/template_index/footer.blade.php
@@ -0,0 +1,84 @@
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-newsletter">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6">
+            <h4>Saran atau Masukan</h4>
+            <p>Silahkan kirim saran atau masukan Anda demi kemajuan grup riset kami.</p>
+            <form action="" method="post">
+              <input type="text" name="saranataumasukan"><input type="submit" value="Kirim">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-6 col-md-6 footer-contact">
+            <h3>DPRMLab</h3>
+            <p>
+              Fakultas Ilmu Komputer Universitas Sriwijaya <br>
+              Jl. Raya Palembang - Prabumulih <br>
+              Km. 32 Indralaya, Ogan Ilir, <br>
+              Sumatera Selatan <br><br>
+              <strong>Email :</strong> dprmlab2020@gmail.com <br>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Topik Penelitian Kami</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Data Science</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Machine Learning</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Cryptography</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Tetap Terhubung Dengan Kami</h4>
+            <p>Berikut tautan sosial media dari grup riset kami.</p>
+            <div class="social-links mt-3">
+              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              <a href="#" class="github"><i class="bx bxl-github"></i></a>
+              <a href="mailto:dprmlab2020@gmail.com" class="github"><i class="bx bx-mail-send"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container py-4">
+      <div class="copyright">
+        Copyright &copy; 2021 <strong><span>DPRMLab</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/ -->
+        Web Template Inspired by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
