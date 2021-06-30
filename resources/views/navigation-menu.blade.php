7a64ee5f4eecbbdac35e9d36a909d64bccde9a52
aldiariq <aldi48767@gmail.com>
2021-06-30 12:46:58 +0700

    finish menu, page and controller for dashboard

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -16,6 +16,36 @@
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('pembimbing.index') }}" :active="request()->routeIs('pembimbing.index')">
+                        {{ __('Dosen Pembimbing') }}
+                    </x-jet-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('penelitian.index') }}" :active="request()->routeIs('penelitian.index')">
+                        {{ __('Penelitian Kami') }}
+                    </x-jet-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('anggota.index') }}" :active="request()->routeIs('anggota.index')">
+                        {{ __('Anggota Kami') }}
+                    </x-jet-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('bimbingan.index') }}" :active="request()->routeIs('bimbingan.index')">
+                        {{ __('Jadwal Bimbingan') }}
+                    </x-jet-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('saranmasukan.index') }}" :active="request()->routeIs('saranmasukan.index')">
+                        {{ __('Saran atau Masukan') }}
+                    </x-jet-nav-link>
+                </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\PembimbingController;
+use App\Http\Controllers\PenelitianController;
+use App\Http\Controllers\AnggotaController;
+use App\Http\Controllers\BimbinganController;
+use App\Http\Controllers\SaranmasukanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +24,9 @@ Route::get('/', [IndexController::class, 'index']);
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::resource('pembimbing', PembimbingController::class);
+Route::resource('penelitian', PenelitianController::class);
+Route::resource('anggota', AnggotaController::class);
+Route::resource('bimbingan', BimbinganController::class);
+Route::resource('saranmasukan', SaranmasukanController::class);
