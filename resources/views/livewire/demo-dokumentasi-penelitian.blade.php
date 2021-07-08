67ee63ee15d0b276f26d49fccc196deabb3382a4
aldiariq <aldi48767@gmail.com>
2021-07-08 14:14:43 +0700

    pull from main

---
+++ resources/views/livewire/demo-dokumentasi-penelitian.blade.php
@@ -0,0 +1,122 @@
+<div>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Demo/Dokumentasi Penelitian') }}
+        </h2>
+    </x-slot>
+    <div class="py-6">
+        @if($modal)
+        @include('livewire.modal.modaldemodokumentasipenelitian')
+        @endif
+
+        @if($modaldelete)
+        <div class="fixed z-10 inset-0 overflow-y-auto ease-out duration-400">
+            <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div class="fixed inset-0 transition-opacity">
+                    <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+                </div>
+                <!-- This element is to trick the browser into centering the modal contents. -->
+                <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>​
+
+                <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                    <h3 class="font-semibold text-xl text-gray-800 leading-tight px-3 py-6 text-center">
+                        {{ __('Ingin Menghapus Data?') }}
+                    </h3>
+                    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                        <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+                            <button wire:click.prevent="delete({{$id_demo_dokumentasi_penelitian}})" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                                Hapus
+                            </button>
+                        </span>
+                        <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+                            <button wire:click="closeModal()" type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                                Batal
+                            </button>
+                        </span>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endif
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden sm:rounded-lg">
+                <div class="min-w-screen justify-center min-h-screen bg-gray-100 flex justify-center bg-gray-100 font-sans overflow-hidden">
+
+                    <div class="w-full lg:w-5/6 px-2">
+                        <button wire:click="showModal()" class="w-full h-10 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
+                            <h2 class="font-semibold text-sm text-white-800 leading-tight">
+                                {{ __('Tambah Demo/Dokumentasi Penelitian') }}
+                            </h2>
+                        </button>
+                        <div class="bg-white shadow-md rounded my-6 overflow-x-scroll">
+                            <table class="min-w-max w-full table-auto">
+                                <thead>
+                                    <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                                        <th class="py-3 px-6 text-left">Judul</th>
+                                        <th class="py-3 px-6 text-left">Nama</th>
+                                        <th class="py-3 px-6 text-center">Keterangan</th>
+                                        <th class="py-3 px-6 text-center">Video</th>
+                                        <th class="py-3 px-6 text-center">Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="text-gray-600 text-sm font-light">
+                                    @forelse ($demodokumentasi_penelitian as $data)
+                                    <tr class="border-b border-gray-200 hover:bg-gray-100">
+                                        <td class="py-3 px-6 text-left whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <span class="font-medium ">{{ $data->judul_penelitian }}</span>
+                                            </div>
+                                        </td>
+                                        <td class="py-3 px-6 text-left">
+                                            <div class="flex items-center">
+                                                <span class="font-medium">{{ $data->nama_penelitian }}</span>
+                                            </div>
+                                        </td>
+                                        <td class="py-3 px-6 text-left">
+                                            <div class="flex items-center">
+                                                <span class="font-medium">{{ $data->ket_demo_dokumentasi_penelitian }}</span>
+                                            </div>
+                                        </td>
+                                        <td class="py-3 px-6 text-left">
+                                            <div class="flex items-center">
+                                                <span class="font-medium">{{ $data->linkvideo_demo_dokumentasi_penelitian }}</span>
+                                            </div>
+                                        </td>
+                                        <td class="py-3 px-6 text-center">
+                                            <div class="flex item-center justify-center">
+                                                <button wire:click="edit({{ $data->id_demo_dokumentasi_penelitian }})" class="flex text-sm border-2 border-transparent transition">
+                                                    <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                                        </svg>
+                                                    </div>
+                                                </button>
+                                                <button wire:click="showModaldelete({{ $data->id_demo_dokumentasi_penelitian }})" class="flex text-sm border-2 border-transparent transition">
+                                                    <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                        </svg>
+                                                    </div>
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    @empty
+                                    <h2 class="font-semibold text-sm text-center text-gray-800 leading-tight">
+                                        {{ __('Data Tidak Ditemukan') }}
+                                    </h2>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="py-3">
+                            {{ $demodokumentasi_penelitian->links() }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
